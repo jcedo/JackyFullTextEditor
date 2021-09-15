0724d41ab48cf5cfd04dfac5baa5d5fa56b8fbd7
jcedo <350477631@qq.com>
2021-09-16 07:11:55 +0800

    chore: development environment init

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -7,4 +9,25 @@ module.exports = {
     path: path.resolve(__dirname, 'dist')
   },
   mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          "style-loader",
+          "css-loader"
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "TEST"
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: "src/public"}
+      ]
+    })
+  ]
 };
